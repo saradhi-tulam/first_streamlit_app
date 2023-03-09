2bd9a076feff3fedfaf9cf27de23712b4b758648
saradhi-tulam <126646742+saradhi-tulam@users.noreply.github.com>
2023-03-09 14:38:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,6 +50,6 @@ streamlit.text(my_data_row)
 
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("Fruit load list contains")
-streamlit.text(my_data_row)
+streamlit.header("Fruit load list contains")
+streamlit.dataframe(my_data_row)
 
